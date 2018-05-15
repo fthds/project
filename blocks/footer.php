fc2227edbd47ccfba93e44037113f45eee1adc34
te3ska <te3ska@bk.ru>
2018-05-15 22:52:29 +0300

    first commit

---
+++ blocks/footer.php
@@ -0,0 +1,3 @@
+<section class="footer">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+</section>
---
+++ blocks/header.php
@@ -0,0 +1,20 @@
+<div class="container">
+    <div class="row">
+        <div class="col l4 xl4 left-side">
+            <span>left</span>
+        </div>
+        <div class="col l8 xl8 main-block">
+            <div class="log">
+                <div class="additionally">
+                    <i class="material-icons">
+                        menu
+                    </i>
+                    Дополнительно
+                </div>
+                <span class="regg">Регистрация</span>
+                <span class="login">
+                    <i class="material-icons key-log">vpn_key</i> Вход</span>
+            </div>
+        </div>
+    </div>
+</div>
